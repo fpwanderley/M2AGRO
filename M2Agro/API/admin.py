09a550e70c965a623cc77f47189e37b4529b780b
Felipe Wanderley <fpwanderley@gmail.com>
2017-02-04 11:51:55 -0300

    - Configurando a interface Admin para o model Harvest.

--- M2Agro/API/admin.py
+++ M2Agro/API/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from models import Product
+from models import Product, Harvest
 
 
 @admin.register(Product)
@@ -8,6 +8,16 @@ class ProductAdmin(admin.ModelAdmin):
 
     # Displayed fields in the models list.
     list_display = ('id', 'name')
+
+    # Fields used for editing.
+    list_display_links = ('name',)
+
+
+@admin.register(Harvest)
+class HarvestAdmin(admin.ModelAdmin):
+
+    # Displayed fields in the models list.
+    list_display = ('id', 'name', 'initial_date', 'final_date')
 
     # Fields used for editing.
     list_display_links = ('name',)
